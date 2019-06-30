7b1824ef6ece0e9f1f5670077c5e7cfc033ba5ed
NOP REsearcher <nopresearcher@gmail.com>
2019-06-30 16:29:05 -0400

    initial

---
+++ setup.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+# icons
+# ❌⏳💀🎉 ℹ️ ⚠️
+
+# run update and upgrade, before running script
+# apt update && apt upgrade -y
+# curl -L https://bit.ly/320yIij | bash
+
+# TODO 
+# systemctl enable sshd
+# sed sshd_config for permit root login and password, x11 forwarding, etc
+# systemctl restart sshd
+
+# set colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE="\033[01;34m" # Heading
+NO_COLOR='\033[0m'
+CLEAR_LINE='\r\033[K'
+BOLD="\033[01;01m" # Highlight
+RESET="\033[00m" # Normal
+
+# set env variable for apt-get installs
+export DEBIAN_FRONTEND=noninteractive
+
+# verify running as root
+if [[ "${EUID}" -ne 0 ]]; then
+  echo -e ' '${RED}'[!]'${RESET}" This script must be ${RED}run as root${RESET}" 1>&2
+  echo -e ' '${RED}'[!]'${RESET}" Quitting..." 1>&2
+  exit 1
+else
+  echo -e " ${BLUE}[*]${RESET} ${BOLD}Starting Kali setup script${RESET}"
+fi
+
+
+compute_start_time(){
+    start_time=$(date +%s)
+}
+
+apt_update() {
+    printf "  ⏳  apt-get update\n"
+    apt-get update -qq >> script.log 2>>script_error.log
+}
+
+apt_upgrade() {
+    printf "  ⏳  apt-get upgrade\n"
+    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq >> script.log 2>>script_error.log
+}
+
+apt_package_install() {
+    echo "\n [+] installing $1 via apt-get\n" >> script.log
+    apt-get install -y -qq --print-uris $1 >> script.log 2>>script_error.log
+}
+
+kali_metapackages() {
+    printf "  ⏳  install Kali metapackages\n"
+    for package in kali-linux-forensic kali-linux-pwtools kali-linux-rfid kali-linux-sdr kali-linux-voip kali-linux-web kali-linux-wireless forensics-all
+    do
+        apt_package_install $package
+    done
+}
+
+install_kernel_headers() {
+    echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}kernel headers${RESET}"
+    apt -y -qq install make gcc "linux-headers-$(uname -r)" \
+    || printf ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+    if [[ $? != 0 ]]; then
+    echo -e ' '${RED}'[!]'${RESET}" There was an ${RED}issue installing kernel headers${RESET}" 1>&2
+        printf " ${YELLOW}[i]${RESET} Are you ${YELLOW}USING${RESET} the ${YELLOW}latest kernel${RESET}?"
+        printf " ${YELLOW}[i]${RESET} ${YELLOW}Reboot${RESET} your machine"
+    fi
+}
+
+folder_prep(){
+    # create folders for later installs and workflow
+    printf "  ⏳  making directories\n"
+    mkdir -p /root/git
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log 
+    fi
+    mkdir -p /root/utils
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log 
+    fi
+    mkdir -p /root/dev
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log 
+    fi
+    mkdir -p /root/share
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log 
+    fi 
+}
+
+github_desktop() {
+    printf "  ⏳  Github desktop - fork for linux https://github.com/shiftkey/desktop/releases\n"
+    cd /root/Downloads
+    wget --quiet https://github.com/shiftkey/desktop/releases/download/release-1.6.6-linux2/GitHubDesktop-linux-1.6.6-linux2.deb
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log
+    fi
+    apt_package_install ./GitHubDesktop-linux*
+    rm -f ./GitHubDesktop-linux*
+}
+
+vscode() {
+     printf "  ⏳  Installing VS Code\n"
+    # Download the Microsoft GPG key, and convert it from OpenPGP ASCII 
+    # armor format to GnuPG format
+    curl --silent https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log
+    fi
+    # Move the file into your apt trusted keys directory (requires root)
+    mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+    # Add the VS Code Repository
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+
+    # Update and install Visual Studio Code 
+    apt_update
+    apt_package_install code
+}
+
+install_rtfm(){
+    printf "  ⏳  Installing RTFM\n"
+    git clone --quiet https://github.com/leostat/rtfm.git /opt/rtfm/
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log
+    fi
+    chmod +x /opt/rtfm/rtfm.py; /opt/rtfm/rtfm.py -u
+    sed -i '/export PATH/s/$/\/opt\/rtfm:/' /root/.bashrc
+}
+
+install_docker(){
+    printf "  ⏳  Installing docker\n"
+    curl -fsSL --silent https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log
+    fi
+    echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+    sleep 1s
+    apt_update
+    sleep 1s
+    echo "\n [+] installing docker-ce via apt-get\n" >> script.log
+    apt-get install -y -q docker-ce >> script.log 2>>script_error.log
+    sleep 1s
+    systemctl enable docker
+    docker version
+    if [[ $? != 0 ]]; then
+	    printf "${CLEAR_LINE}❌${RED} $1 failed ${NO_COLOR}\n"
+        echo "$1 failed " >> script.log
+    fi
+}
+
+bash_aliases() {
+    # git aliases
+    echo "alias gs='git status'" >> /root/.bashrc
+    echo "alias ga='git add -A'" >> /root/.bashrc
+    # increase history size
+    sed -i "s/HISTSIZE=1000/HISTSIZE=100000/g" /root/.bashrc
+    # establish path baseline
+    echo "export PATH=$PATH:" >> /root/.bashrc
+}
+
+john_bash_completion() {
+    printf "  ⏳  enabling john bash completion\n"
+    echo ". /usr/share/bash-completion/completions/john.bash_completion" >> /root/.bashrc
+}
+
+compute_finish_time(){
+    printf "  ⏳  Time taken\n"
+    finish_time=$(date +%s)
+    echo -e "${YELLOW}[i]${RESET} Time (roughly) taken: ${YELLOW}$(( $(( finish_time - start_time )) / 60 )) minutes${RESET}"
+}
+
+main () {
+    compute_start_time
+    apt_update
+    #apt_upgrade
+    kali_metapackages
+    install_kernel_headers
+    folder_prep
+    github_desktop
+    vscode
+    install_rtfm
+    install_docker
+    bash_aliases
+    john_bash_completion
+    compute_finish_time
+}
+
+main    
